154f30ae91728d989695ccf2a86d955770edc273
Mukamyi izere Arcange <mukamyiizerearcange@gmail.com>
2026-09-02 21:08:18 +0200

    feat: enable V3 admin enhancements

--- apps/admin/src/main.jsx
+++ apps/admin/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import './v3.css';
+import './v3Enhancements.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
