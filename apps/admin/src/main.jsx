7d15c2abc054e05a0e0130c720140e3f2a93b979
Mukamyi izere Arcange <mukamyiizerearcange@gmail.com>
2026-09-04 17:40:51 +0200

    feat(admin): enable V4 portfolio CMS enhancements

--- apps/admin/src/main.jsx
+++ apps/admin/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './v3.css';
 import './v3Enhancements.js';
+import './v4Enhancements.js';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
